e303f5ad14ec4165101098d948d87adfb8add6f6
SaiSunil616161 <saisunil616161@gmail.com>
2021-08-29 12:12:44 +0530

    changed backend url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const MAIN_URL = "http://userappnew-env.eba-gyvmk7ct.us-east-2.elasticbeanstalk.com/api/users";
+export const MAIN_URL = "http://ledgerapplication-env.eba-4emntayk.us-east-2.elasticbeanstalk.com/api/users";
 // export const MAIN_URL = "http://localhost:5000/api/users";
